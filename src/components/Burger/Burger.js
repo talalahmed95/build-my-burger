0fccdb6f6d2f1f04d3b261731e488d0f2152c400
Talal <tahmed530@gmail.com>
2020-09-10 21:44:41 +0500

    reduce(): ingredients in single output

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,22 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-	const transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
-		return [...Array(props.ingredients[ingKey])].map((_, i) => {
-			return <BurgerIngredient key={ingKey + i} type={ingKey} />
-		});
-	});
-
 	// Object.keys converts keys into array, and map on each element of array. 
 	// props.ingredients[ingKey] is the value like 1,2 and [...Array] converts it into array of values [1,1,2,2]
 	// _ is empty length, i is index i.e 0 and 1
+	let transformedIngredients = Object.keys(props.ingredients)
+		.map(ingKey => {
+			return [...Array(props.ingredients[ingKey])]
+			.map((_, i) => {
+				return <BurgerIngredient key={ingKey + i} type={ingKey} />
+			});
+		})
+		.reduce((arr, el) => {return arr.concat(el)}, []);
+		// reduce takes 'arr' as accumulator and 'el' as element and '[]' as initial value
+
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please add ingredients!</p>; 
+	}
 
 	return (
 		<div className={classes.Burger}>
