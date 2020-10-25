324d7d44aa31c02cbc3d25f54855d0e7977c341f
Talal <tahmed530@gmail.com>
2020-10-25 11:17:33 +0500

    optimized modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,7 +1,10 @@
 import React, {Fragment} from 'react';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+const OrderSummary = (props) => {
+	
+	console.log('OrderSummary updated');
+	
 	const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
 		return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>;
 	} );
@@ -21,4 +24,4 @@ const orderSummary = (props) => {
 	);
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, {Fragment, memo} from 'react';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => {
+const Modal = (props) => {
+
+	console.log('Modal updated');
 
 	let modalState = props.show ? classes.Show : classes.Hide;
 
 	return (
-		<>
+		<Fragment>
 			<Backdrop show={props.show} />
 			<div className={classes.Modal + ' ' + modalState}>
 				{props.children}
 			</div>
-		</>
+		</Fragment>
 	);
 }
 
-export default modal;
+const shouldUpdate = (prevProps, nextProps) => {
+	return prevProps.show === nextProps.show;
+}
+
+export default memo(Modal, shouldUpdate);
